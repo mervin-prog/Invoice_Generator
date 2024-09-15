32adb531d13fda526bbe9cfe1c7cec064e8e5658
mervin <mervinrenie@gmail.com>
2024-09-15 15:03:31 +0530

    deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Invoice_Generator',
+  base: '/Invoice_Generator/',
   build: {
     outDir: 'build'
   }
